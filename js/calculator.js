24ad9789bf2bf47949239f0a9a05d450d7e06819
dpkolee <dpkolee@gmail.com>
2017-04-22 11:49:43 +0545

    added multiply and divide function

--- js/calculator.js
+++ js/calculator.js
@@ -1,43 +1,95 @@
-window.addEventListener('load', handleLoad);
-
-function handleLoad() {
-    var calculateButton = document.querySelector('#calculate');
-    var clearButton = document.querySelector('#clear');
-
-    calculateButton.addEventListener('click', handleCalculateClick);
-    clearButton.addEventListener('click', handleClearClick);
-
+ window.addEventListener('load', handleLoad);
+ 
+ var op = '+'; // The calculation operator
+ 
+ function handleLoad() {
+     var calculateButton = document.querySelector('#calculate');
+     var clearButton = document.querySelector('#clear');
+     var minusButton = document.querySelector('#minus');
+     var plusButton = document.querySelector('#plus');
+     var multiplyButton = document.querySelector('#multiply');
+     var divideButton = document.querySelector('#divide'); 
+ 
+     calculateButton.addEventListener('click', handleCalculateClick);
+     clearButton.addEventListener('click', handleClearClick);
+     minusButton.addEventListener('click', handleMinusClick);
+     plusButton.addEventListener('click', handlePlusClick);
+     multiplyButton.addEventListener('click', handleMultiplyClick);
+     divideButton.addEventListener('click', handleDivideClick);
 }
+    function handlePlusClick() {
+    console.log('Plus button clicked.');
+    var operator = document.querySelector('#operator');
+    op = '+';
+    operator.innerText = '+';
+    }
+ 
+ function handleMinusClick() {
+     console.log('Minus button clicked.');
+ 
+     var operator = document.querySelector('#operator');
+ 
+     op = '-';
+     operator.innerText = '-';
+ }
 
-function handleCalculateClick() {
-    console.log('Calculate Button Clicked.');
+ function handleMultiplyClick(){
+    console.log('multiply button clicked');
+    var operator = document.querySelector('#operator');
+    op = '*';
+    operator.innerText = '*';
+ }
 
-    // Get the input & result elements
-    var input1 = document.querySelector('#input1');
-    var input2 = document.querySelector('#input2');
-    var output = document.querySelector('#output');
-
-    // Get the values.
-    var value1 = Number(input1.value);
-    var value2 = Number(input2.value);
-
-    // Just add those two values.
-    var result = value1 + value2;
-
-    // Update the output element's content.
-    output.innerHTML = result;
-}
-
-    function handleClearClick() {
-    console.log('Clear Button Clicked.');
-
-    // Get the input & result elements.
-    var input1 = document.querySelector('#input1');
-    var input2 = document.querySelector('#input2');
-    var output = document.querySelector('#output');
-
-    // Clear the fields and output.
-    input1.value = '';
-    input2.value = '';
-    output.innerHTML = '';
+ function handleDivideClick(){
+    console.log('divide button clicked');
+    var operator = document.querySelector('#operator');
+    op = '/';
+    operator.innerText = '/';
+ }
+ 
+ function handleCalculateClick() {
+     console.log('Calculate Button Clicked.');
+ 
+     // Get the input & result elements
+     var input1 = document.querySelector('#input1');
+     var input2 = document.querySelector('#input2');
+     var output = document.querySelector('#output');
+ 
+     // Get the values.
+     var value1 = Number(input1.value);
+     var value2 = Number(input2.value);
+ 
+     // Just add those two values.
+     var result = calculate(value1, value2);
+ 
+     // Update the output element's content.
+     output.innerHTML = result;
+ }
+ 
+ function calculate(value1, value2) {
+     switch (op) {
+        case '+':
+             return value1 + value2;
+ 
+        case '-':
+             return value1 - value2;
+        case '*':
+             return value1 * value2;
+        case '/':
+             return value1 / value2;
+     }
+ }
+ 
+ function handleClearClick() {
+     console.log('Clear Button Clicked.');
+ 
+     // Get the input & result elements.
+     var input1 = document.querySelector('#input1');
+     var input2 = document.querySelector('#input2');
+     var output = document.querySelector('#output');
+ 
+     // Clear the fields and output.
+     input1.value = '';
+     input2.value = '';
+     output.innerHTML = '';
 }
